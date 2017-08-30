3313d7b4ded76f40abcb64c192d09033610ae9b3
Jashion <ycyz138258886@qq.com>
2017-08-30 22:23:40 +0800

    Project init

---
+++ SDTextAlignmentLabel/SDTextAlignmentLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDTextAlignmentLabel
+//
+//  Created by Jashion on 2017/8/30.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDTextAlignmentLabel/SDTextAlignmentLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDTextAlignmentLabel
+//
+//  Created by Jashion on 2017/8/30.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
